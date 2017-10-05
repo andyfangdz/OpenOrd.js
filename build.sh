3e369649d406119e4ba7673206acb5b6477f2169
Andy Fang <andyfang.dz@gmail.com>
2017-10-05 03:46:47 -0400

    switch to WASM

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf dist bin
 mkdir bin
 
-emcc src/DensityGrid.cpp src/graph.cpp -std=c++11 -Wall -o bin/OpenOrd.js -O3 --bind -s TOTAL_MEMORY=1073741824
+emcc src/DensityGrid.cpp src/graph.cpp -std=c++11 -Wall -o bin/OpenOrd.js -O3 -ffast-math --bind -s TOTAL_MEMORY=1073741824 -s WASM=1
 
 cp -a bin/. dist
 cp -a bin/. docs
